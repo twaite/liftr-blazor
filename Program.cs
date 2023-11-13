657adcc84892dd8c7ec1a0eafea2fc8ba6c709b3
Timothy Waite <tim@the-waite.com>
2023-11-13 00:13:37 -0600

    Setup Tailwind for styling

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Liftr.Components;
 using Microsoft.EntityFrameworkCore;
+using Tailwind;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+if (app.Environment.IsDevelopment())
+{
+    app.RunTailwind("tailwind", "./");
+}
+
 
 app.UseHttpsRedirection();
 
