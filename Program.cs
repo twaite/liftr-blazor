8439eb9148aeabff5dd1b72eaf1438c0861fe967
Timothy Waite <tim@the-waite.com>
2023-11-19 01:28:13 -0600

    WIP on auth, added a login page

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Liftr.Components;
 using Microsoft.EntityFrameworkCore;
 using Tailwind;
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Authentication.Google;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +11,23 @@ builder.Services.AddDbContextFactory<LiftrContext>(opt =>
     opt.UseNpgsql(@"Host=localhost;Username=user;Password=password;Database=liftr_db;Port=5433"));
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
+builder.Services
+    .AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = BearerTokenDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+    })
+    .AddBearerToken()
+    .AddGoogle(options =>
+    {
+        options.ClientId = "1063057726088-3svpnq2q35nq7738k20o44phu3gdtb3i.apps.googleusercontent.com";
+        options.ClientSecret = "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";
+        options.SaveTokens = true;
+    });
 
 var app = builder.Build();
 
@@ -27,9 +44,9 @@ if (app.Environment.IsDevelopment())
     app.RunTailwind("tailwind", "./");
 }
 
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
